e076a5e9c90f1faeb676a1fd19db8acf34e90b4c
Rita <ritamukimba@gmail.com>
2017-04-26 15:35:09 +0300

    Add person to dojo functions defined

--- class_models/the_dojo.py
+++ class_models/the_dojo.py
@@ -1,6 +1,9 @@
 from class_models.room import Room
 from class_models.office import Office
 from class_models.living_space import LivingSpace
+from class_models.person import Person
+from class_models.fellow import Fellow
+from class_models.staff import Staff
 
 class Dojo(object):
 
@@ -10,13 +13,13 @@ class Dojo(object):
         self.no_of_rooms = 0
         self.list_rooms = []
         self.max_no_rooms = 50
+        self.max_no_people = 120
         self.list_people = []
 
-
-    def create_room(self, room_name, roomtype):
+    def create_room(self, room_name, room_type):
         if len(self.list_rooms) < self.max_no_rooms:
             if not self.room_exists(room_name):
-                if roomtype == "office":
+                if room_type == "office":
                     office = Office(room_name)
                     self.list_rooms.append(office)
                     return "An office called "+room_name+" has been successfully created!"
@@ -34,5 +37,28 @@ class Dojo(object):
     def room_exists(self, room_name):
         for room in self.list_rooms:
             if room.room_name == room_name:
+                return True
+        return False
+
+    def add_person(self, person_name, person_type):
+        if len(self.list_people) < self.max_no_people:
+            if not self.person_exists(person_name):
+                if person_type == "staff":
+                    staff = Staff(person_name)
+                    self.list_people.append(staff)
+                    return "An person called " + person_name + " has been successfully added!"
+
+                elif person_type == "fellow":
+                    fellow = Fellow(person_name)
+                    self.list_people.append(fellow)
+                    return "A person called " + person_name + " has been successfully created!"
+            else:
+                return " Person " + person_name+ " already exists"
+        else:
+            return "Sorry! This Dojo is full"
+
+    def person_exists(self, person_name):
+        for person in self.list_people:
+            if person.person_name == person_name:
                 return True
         return False
