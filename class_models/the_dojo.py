6db17bf7cd30604f33f11711ef25cc512d421a67
Rita <ritamukimba@gmail.com>
2017-04-26 10:11:27 +0300

    Create function to verify if room to be created already exists

--- class_models/the_dojo.py
+++ class_models/the_dojo.py
@@ -9,11 +9,13 @@ class Dojo(object):
     def __init__(self):
         self.no_of_rooms = 0
         self.list_rooms = []
+        self.max_no_rooms = 50
         self.list_people = []
 
 
     def create_room(self, room_name, roomtype):
         if len(self.list_rooms) < self.max_no_rooms:
+            if not self.room_exists(room_name):
                 if roomtype == "office":
                     office = Office(room_name)
                     self.list_rooms.append(office)
@@ -24,8 +26,15 @@ class Dojo(object):
                     self.list_rooms.append(livingspace)
                     #self.list_rooms.append(Office(room_name))
                     return "A living space called "+room_name+" has been successfully created!"
+            else:
+                return "Room "+room_name+" already exists"
         else:
             return "The Dojo has no extra rooms to add"
 
-        # def all_rooms(self):
+    def room_exists(self, room_name):
+        for room in self.list_rooms:
+            if room.room_name == room_name:
+                return True
+        return False
+    # def all_rooms(self):
     #     return self.list_rooms
