3b767ea8092596cba276bab5a3342b32fad254e4
Rita <ritamukimba@gmail.com>
2017-04-26 16:35:07 +0300

    Create a class which defines a fellow

--- class_models/fellow.py
+++ class_models/fellow.py
@@ -1,8 +1,11 @@
 from class_models.person import Person
-class Fellow(object):
-    """
-        Fellows in Dojo
-    """
-    fellow_people =[]
 
-    def __init__(self):
+class Fellow(Person):
+    """
+        A type of person in the Dojo
+    """
+    list_fellows = []
+
+    def __init__(self, person_name):
+        super().__init__(person_name)
+        self.wants_livingspace = False
